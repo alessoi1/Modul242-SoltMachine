bea98fddc00ba0593aa5ab84cefddc29c8513420
xxD04xx <dominic.ritzmann@noseryoung.ch>
2023-06-20 11:33:54 +0200

    gesture sensor added

--- core2imagedemo/src/main.cpp
+++ core2imagedemo/src/main.cpp
@@ -7,10 +7,12 @@
 #include "images/image_grapes.h"
 #include "images/image_watermelon.h"
 #include "ImageRoller.h"
+#include "DFRobot_PAJ7620U2.h"
 
 
 void event_handler_checkbox(struct _lv_obj_t * obj, lv_event_t event);
 void event_handler_button(struct _lv_obj_t * obj, lv_event_t event);
+void event_handler_gesture(struct _lv_obj_t * obj, lv_event_t event);
 void init_image_roller();
 void mqtt_callback(char* topic, byte* payload, unsigned int length);
 
@@ -18,25 +20,26 @@ unsigned long next_lv_task = 0;
 
 ImageRoller * rip = new ImageRoller();
 
+DFRobot_PAJ7620U2 gestureSensor;
+
 void event_handler_button(struct _lv_obj_t * obj, lv_event_t event) {
   if(event == LV_EVENT_PRESSED && rip->is_stopped()) {
     rip->start();
   }
 }
 
-
 void init_image_roller() {
   rip->add_image(add_image(&image_cherry, 0, 0, event_handler_button));
   rip->add_image(add_image(&image_lemon, 0, 0, event_handler_button));
   rip->add_image(add_image(&image_grapes, 0, 0, event_handler_button));
   rip->add_image(add_image(&image_watermelon, 0, 0, event_handler_button));
+   
 }
 
 
 // ----------------------------------------------------------------------------
 // MQTT callback
 // ----------------------------------------------------------------------------
-
 void mqtt_callback(char* topic, byte* payload, unsigned int length) {
   // Parse Payload into String
   char * buf = (char *)malloc((sizeof(char)*(length+1)));
@@ -83,6 +86,13 @@ void loop() {
   }
   // Uncomment the following lines to enable MQTT
   //mqtt_loop();
+
+  // Check for gesture recognition
+    DFRobot_PAJ7620U2::eGesture_t gesture = gestureSensor.getGesture();
+    if (gesture != gestureSensor.eGestureNone) {
+      rip->start();
+    }
+    mqtt_publish("imageroller/gesture", String("spin").c_str());
 }
 
 // ----------------------------------------------------------------------------
@@ -105,7 +115,7 @@ void setup() {
   
   // Uncomment the following lines to enable WiFi and MQTT (and the mqtt_loop() line in the loop function)
   //init_networking();
-
+  gestureSensor.begin();
   init_image_roller();
   init_sideled();
   rip->start();
